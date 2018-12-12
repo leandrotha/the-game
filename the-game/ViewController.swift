8fad1cde45db2e8308a17fd844a79e3798d6787a
Leandro <leandro.tha@livetouch.com.br>
2018-12-12 14:23:44 -0200

    Initial Commit

---
+++ the-game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  the-game
+//
+//  Created by Livetouch on 12/12/18.
+//  Copyright © 2018 Leandro B Tha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
